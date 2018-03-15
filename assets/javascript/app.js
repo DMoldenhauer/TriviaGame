83af051984fd7e1a63b97061bf0b5939d63ea635
Don Moldenhauer <dmmoldenhauer@gmail.com>
2018-03-14 21:59:29 -0500

    game works needs styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,15 @@
 //-------------------------------------------
 
 // You'll create a trivia form with multiple choice or 
-    //true/false options (your choice).
+//true/false options (your choice).
 
 // The player will have a limited amount of time to 
-    //finish the quiz. 
+//finish the quiz. 
 
 
 // The game ends when the time runs out. The page will 
-    //reveal the number of questions that players answer 
-    //correctly and incorrectly.
+//reveal the number of questions that players answer 
+//correctly and incorrectly.
 
 
 // Don't let the player pick more than one answer per question.
@@ -18,59 +18,118 @@
 
 //PSEUDOCODE
 //----------------------------------------------------------------
-    //when game starts
-        // event handler for START button click
-        // replace start button with quiz questions and DONE button
-        // start timer
-        // display timer
-        // track correct and incorrect answers
-        //IF player press DONE before the timer expires
-            //switch to score display
-        //ELSE 
-            //switch to score display when timer expires
+//when game starts
+// event handler for START button click
+// replace start button with quiz questions and DONE button
+// start timer
+// display timer
+// track correct and incorrect answers
+//IF player press DONE before the timer expires
+//switch to score display
+//ELSE 
+//switch to score display when timer expires
 
 //GLOBAL VARIABLES
 //-------------------------------------------------------------
 
-var correctAnswerCount = null;
-var incorrectAnswerCount = null;
-var timeIsUp = false;
+var correctAnswerCount = 0;
+var incorrectAnswerCount = 0;
+var unansweredCount = 0;
+var totalQuestions = 8;
+var time = 30;
+
+
 
 //MAIN PROCESS
 //------------------------------------------------------------
-$(function ()
-{
+$(function () {
+    $("#questions").hide();
+    $("#results").hide();
     //waits for click event handler on START button to call function startGame()
-startGame();
+    $("#startButton").on("click", function () {
+        //Testing
+        console.log("start button clicked");
+        startGame();
+        //Testing
+    });
+    $("#endButton").on("click", function () {
+        //    //Testing
+        console.log("end button clicked");
+        endGame();
+        //Testing
+    });
 });
 
 
 //FUNCTIONS
 
-function startGame()
-{
+function startGame() {
     //switches html in quizBoard to replace start button with question list
     //starts timer()
-    //calls questionAnswerCount() 
+    //Testing
+    console.log("startGame() ran");
 
-function questionAnswerCount ()
+    $("#startButton").hide();
+    $("#questions").show();
+    window.setTimeout(endGame, 30000);
+    timerInterval = setInterval(updateTime, 1000);
+    $("#displayTimeRemain").text("Time Remaining: " + time);
+    //Testing
+    console.log(time);
+    questionAnswerCount();
+
+}
+
+function updateTime() 
 {
+    time--;
+
+    $("#displayTimeRemain").text("Time Remaining: " + time);
+
+    //testing
+    console.log(time);
+    if (time < 1) 
+    {
+        clearInterval(timerInterval);
+       
+    }
+}
+
+function questionAnswerCount() {
     //waits for input event handler on questions
+    // $(".question").on("click", function ()
+    //     //Testing
+    // {   console.log (".question form clicked");
     //compares input to correct answers and tallies score
-    //IF timeIsUp === true
-        //call endGame()
-    //ELSE
+    $(".correct").on("click", function () {
+        correctAnswerCount++;
+        //Testing 
+        console.log("correctAnswerCount= " + correctAnswerCount);
+    });
+
+    $(".incorrect").on("click", function () {
+        incorrectAnswerCount++;
+        //Testing 
+        console.log("incorrectAnswerCount= " + incorrectAnswerCount);
+    });
+    //Testing
+    console.log("questionAnswerCount() ran");
 }
 
-function timer()
-{
-    //IF timer expires, changes var timeisUP to true  
-}
 
-function endGame()
-{
+function endGame() {
     //switches html in quizBoard div to display of results
+    //changes 
+    $("#questions").hide();
+    $("#results").show();
+    //Testing
+    console.log("endGame() ran");
+    unansweredCount = (totalQuestions - correctAnswerCount - incorrectAnswerCount);
+    $("#correctCountDisplay").text("Correct Answers: " + correctAnswerCount);
+    $("#incorrectCountDisplay").text("Incorrect Answers: " + incorrectAnswerCount);
+    $("#unansweredCountDisplay").text("Unanswered Questions: " + unansweredCount);
 }
 
 
-}
+
+
